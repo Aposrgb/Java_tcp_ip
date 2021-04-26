20d39ee52a1bcbe3d91bcdc2ec114482d1034ddd
Aposrgb <63498564+Aposrgb@users.noreply.github.com>
2021-04-26 14:15:05 +0300

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,30 +1,116 @@
 package client;
 
+import java.util.List;
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client {
 
+    private static Socket s;
+    private static String ctlg_dest = "C:/Users/abbos/Desktop/";
+    private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) throws IOException, IOException {
-        Scanner sc = new Scanner(System.in);
-        Socket s=null ;
+        
         while (true) {
             try {
-                s = new Socket("localhost", 5789);
-                InputStreamReader in = new InputStreamReader(s.getInputStream());
-                BufferedReader bf = new BufferedReader(in);
-                String str = bf.readLine();
-                System.out.println("Server_MSG: " + str + "\n");
+                s = new Socket("localhost", 3000);
+                System.out.println(getMsg() + "\nВ какую папку перейти? Что-то скачать?");// получение директории
+                check_connect();
+                move_ctlg();// перемещение между каталогамии
+
+                System.out.println("end");
+                System.in.read();
             } catch (Exception e) {
-                System.out.println("Ошибка");
+                System.out.println("Ошибка " + e.toString());
             }
 
         }
     }
-    public String getMsg(String str, Socket s){
-        
+
+    
+    public static void move_ctlg() throws IOException {
+        while (true) {
+            String str = sc.nextLine();
+            if (str == "Выйти" || str == "выйти") {
+                s.close();
+                break;
+            }
+            sendMsg(str);
+            check_connect();
+            if (str.contains("cd.")) {
+                System.out.println(getMsg());
+            } else if (str.contains(".")) { // если нету cd то это файл с каким-то расширением .txt к примеру
+                downloads(str);
+                System.out.println(getMsg());
+            } else {
+                System.out.println(getMsg());
+            }
+            check_connect();
+        }
+    }
+
+    public static void downloads(String str) throws IOException {
+        FileOutputStream f = new FileOutputStream(ctlg_dest + str);
+        f.write(getMsgB());// запись файла в путь ctlg_dest с названием str
+        check_connect();
+        f.close();
+        System.out.println("Скачано!\nПуть:" + ctlg_dest + str);
+    }
+
+    public static void check_connect() throws IOException {// проверка соединения
+        if (s.isClosed()) {
+            s = new Socket("localhost", 3000);
+        }
+    }
+
+    public static void sendMsg(String msg) throws IOException {// отправка сообщения на сервер
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.println(msg);
+        pr.flush();
+        if (pr.checkError()) {
+            System.out.println("ОШИБКА ОТПРАВКИ");
+            check_connect();
+            sendMsg(msg);
+        }
+        pr.close();
+    }
+
+    public static String getMsg() throws IOException, IOException {// передаются текстовые файлы
+        InputStreamReader in = new InputStreamReader(s.getInputStream(), "UTF-8");
+        String str = "";
+        char x;
+        while (true) {
+            x = (char) in.read();
+            if (x == '￿') {
+                break;
+            }
+            str += x;
+        }
+        in.close();
         return str;
+    }
+
+    public static byte[] getMsgB() throws IOException, IOException {// передаются не текстовые файлы
+        InputStream in = s.getInputStream();
+        List<Integer> data = new ArrayList<Integer>();
+        int x;
+        while (true) {
+            x = in.read();
+            if (x == -1) {
+                break;
+            }
+            data.add(x);
+        }
+        in.close();
+
+        byte[] result = new byte[data.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = data.get(i).byteValue();
+        }
+        return result;
     }
 
 }
--- Server.java
+++ Server.java
@@ -2,61 +2,147 @@ package server;
 
 import java.net.*;
 import java.io.*;
+import java.util.Base64;
 import java.util.Scanner;
 
 public class Server {
-    static String ctlg_path="C:/Users/abbos/Downloads";
+
+    private static String ctlg_path = "C:/Users";
+    private static Socket s;
+    private static ServerSocket ss;
+    private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) throws IOException {
-        ServerSocket ss = new ServerSocket(5789);
-        Socket s;
-        Scanner sc = new Scanner(System.in);
+        ss = new ServerSocket(3000);
         while (true) {
+            System.out.println("start");
             s = ss.accept();
-            System.out.println("Connected");
-            if(s.isClosed()==false){
-                PrintWriter pr = new PrintWriter(s.getOutputStream());
-                pr.println(sc.next());
-                pr.flush();
-            }
-            s.close();
+            sendMsg(directory());// отправка директории
+            reconnect();
+            move_ctlg();
+
+            System.out.println("end");
+            System.in.read();
         }
     }
-    public static void command (String comm){
-        if(comm=="cd.."){
-            int tmp=0;
-            for(int i=ctlg_path.length()-1;i>0;i--){
-                if(ctlg_path.charAt(i)=='/'){
-                    tmp=i;
-                    break;
-                }
-                else if(i==1){
-                    tmp=0;
-                    break;
-                }
-            }
-            if(tmp!=0){
-                comm="";
-                for(int i=tmp,j=0;i>0;i--,j++){
-                    comm+=ctlg_path.charAt(j);
-                }
-                ctlg_path=comm;
+
+    public static void move_ctlg() throws IOException {// перемещение по папкам 
+        int i = 0;
+        while (true) {
+            String str = getMsg();
+            System.out.println(str);
+            reconnect();
+
+            command(str);
+            sendMsg(directory());
+            reconnect();
+
+            i++;
+            if (i > 30) {
+                break;
             }
         }
     }
-    public static String directory(){
+
+    public static void reconnect() throws IOException {// проверка соединения
+        if (s.isClosed()) {
+            s = ss.accept();
+        }
+    }
+
+    public static void command(String comm) throws IOException {
+        if (comm.contains("cd")) {
+            if (comm.contains("..")) {
+                directory_rollback();
+            } else if (directory().contains("(Папка)" + comm.substring(3, comm.length() - 2))) {
+                ctlg_path += "/" + comm.substring(3, comm.length() - 2);
+            } else if (comm.contains("C:/")) {
+                ctlg_path = comm.substring(comm.indexOf("C:/"), comm.length() - 2);
+            }
+        } else if (directory().contains("(Файл)" + comm.substring(0, comm.length() - 2))) {
+            uploads(comm);
+        }
+    }
+
+    public static void uploads(String comm) throws IOException {
+        InputStream f= new FileInputStream(ctlg_path + "/" + comm.substring(0, comm.length() - 2));
+        OutputStream out = s.getOutputStream();
+        int j;
+        while ((j = f.read()) != -1) {
+            out.write(j);
+        }
+        out.flush();
+        reconnect();
+    }
+
+    public static void directory_rollback() {
+        int tmp = 0;
+        for (int i = ctlg_path.length() - 1; i > 0; i--) {
+            if (ctlg_path.charAt(i) == '/') {
+                tmp = i;
+                break;
+            } else if (i == 1) {
+                tmp = 0;
+                break;
+            }
+        }
+        if (tmp != 0) {
+            String str = "";
+            for (int i = tmp, b = 0; i > 0; i--, b++) {
+                str += ctlg_path.charAt(b);
+            }
+            ctlg_path = str;
+        }
+    }
+
+    public static String directory() {
         File f = new File(ctlg_path);
-        String str ="Путь: "+ctlg_path+"\nКаталог файлов: \n"; 
-        int j=1;
-        for(File i : f.listFiles()){
-            str+=j+") ";
-            if(i.getName().contains(".")){
-                str+=i.getName()+"\n";
+        String str = "\nПуть: " + ctlg_path + "\nКаталог файлов:";
+        int j = 1;
+        try {
+            for (File i : f.listFiles()) {
+                str += "\n" + j + ") ";
+                if (i.getName().contains(".")) {
+                    str += "(Файл)" + i.getName();
+                    str+=" размер "+(double)i.length()/(1024*1024)+" mb";;
+                } else {
+                    str += "(Папка)" + i.getName();
+                }
+                
+                j++;
             }
-            else{
-                str+="(Папка)"+i.getName()+"\n";
-            }
-            j++;
+        } catch (Exception e) {
+            directory_rollback();
+            str = "Извинте этот элемент недоступен!\n" + directory();
         }
+        return str;
+    }
+
+    public static void sendMsg(String msg) throws IOException {
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.print(msg);
+        pr.flush();
+        if (pr.checkError()) {
+            System.out.println("ОШИБКА ОТПРАВКИ");
+        }
+        pr.close();
+
+    }
+    
+   
+
+    public static String getMsg() throws IOException, IOException {
+        InputStreamReader in = new InputStreamReader(s.getInputStream(), "UTF-8");
+        String str = "";
+        char x;
+        while (true) {
+            x = (char) in.read();
+            if (x == '￿') {
+                break;
+            }
+            str += x;
+        }
+        in.close();
         return str;
     }
 }
